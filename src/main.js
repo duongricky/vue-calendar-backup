29cdf5eb8d3f6672f30f3c5a69d82bef5720179a
duongnva-2181 <nguyen.van.anh.duong@sun-asterisk.com>
2020-10-05 08:46:35 +0700

    Done Add, Edit, Remove a event in calendar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+Vue.use(VueTextareaAutosize)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBQU57vaz-x0sGwrNG44oWJ2aQKrpS1Vpg",
+  authDomain: "vuejs-calender-app.firebaseapp.com",
+  databaseURL: "https://vuejs-calender-app.firebaseio.com",
+  projectId: "vuejs-calender-app",
+  storageBucket: "vuejs-calender-app.appspot.com",
+  messagingSenderId: "393857154411",
+  appId: "1:393857154411:web:cb6d839488d13a6cfba02e"
+})
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
