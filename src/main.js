f18804f525c38d65f21cd5c6583f6b938278616b
duongnva-2181 <nguyen.van.anh.duong@sun-asterisk.com>
2020-10-08 13:22:41 +0700

    Config firebase

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ Vue.use(VueTextareaAutosize)
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBQU57vaz-x0sGwrNG44oWJ2aQKrpS1Vpg",
-  authDomain: "vuejs-calender-app.firebaseapp.com",
-  databaseURL: "https://vuejs-calender-app.firebaseio.com",
-  projectId: "vuejs-calender-app",
-  storageBucket: "vuejs-calender-app.appspot.com",
-  messagingSenderId: "393857154411",
-  appId: "1:393857154411:web:cb6d839488d13a6cfba02e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 })
 
 export const db = firebase.firestore();
